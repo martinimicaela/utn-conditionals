47636ec679433ef171c85dbc9d56751f87e98d5a
Micaela Martini <80223363+martinimicaela@users.noreply.github.com>
2021-04-08 16:42:20 -0300

    Add files via upload

---
+++ practice4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+#include <cstdlib>
+
+int main(){
+    int numero1, numero2, diferencia;
+
+    cout << "Ingrese un numero: ";
+    cin >> numero1;
+    cout << "Ingrese otro numero: ";
+    cin >> numero2;
+
+    if (numero1>numero2){
+        diferencia = numero1 - numero2;
+        cout << "La diferencia absoluta entre los dos numeros es " << diferencia;
+    }
+    else {
+        diferencia = numero2 - numero1;
+        cout << "La diferencia absoluta entre los dos numeros es " << diferencia;
+    }
+ system("pause>nul");
+ return 0;
+}
